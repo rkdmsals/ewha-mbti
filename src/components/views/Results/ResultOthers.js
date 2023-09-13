4bde92e067d212aefc35dd5199233196fa50b370
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-09-14 05:40:58 +0900

    이미지 경로 수정, alt 추가

--- src/components/views/Results/ResultOthers.js
+++ src/components/views/Results/ResultOthers.js
@@ -18,7 +18,7 @@ function ResultOthers() {
 
     return (
         <div className="ResultOthers">
-            <div className="ShareText"><img src="img/ResultPage/Buttons/share.png"></img><span>내 결과 공유하기</span></div>
+            <div className="ShareText"><img src="/img/ResultPage/Buttons/share.png"></img><span>내 결과 공유하기</span></div>
             <div className="ShareButtons">
                 <div className="SNSButton"><img
                     src="/img/ResultPage/Buttons/kakao.png"
@@ -41,7 +41,7 @@ function ResultOthers() {
                     className="Images"
                     onClick={ShareLink} /></div>
             </div>
-            <img className="GuideImg" alt="rabbit_guide" src="img/guide_image.png"></img>
+            <img className="GuideImg" alt="rabbit_guide" src="/img/guide_image.png"></img>
             <button className="GoGuideButton">
                 안내 사이트 보러가기</button>
             <button className="AllTypesButton">모든 유형 보러가기</button>
