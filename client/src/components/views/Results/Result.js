4729dbb587ee39cae87840a582fe857e5cad1302
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-09-25 03:17:00 +0900

    결과 모아보기 페이지 이미지 깨진 경우 css수정

--- client/src/components/views/Results/Result.js
+++ client/src/components/views/Results/Result.js
@@ -4,20 +4,23 @@ import ResultOthers from "./ResultOthers";
 import Logo from "../../Footer/Logo";
 import { useParams } from "react-router-dom";
 import resultData from "./results.json";
-// import resultData from "./AllResults.json";
 
+import { useNavigate } from "react-router-dom";
 function Result() {
     let params = useParams();
     let pageData = resultData.results[params.id - 1];
     // console.log(params.id)
+    const navigate = useNavigate();
     return (
         <div className="ResultPage">
             <div className="ResultImageFrame">
+
                 <span className="ResultTitle">나와 어울리는 이화여자대학교 건물은</span>
                 <div className="ResultMiddletext">{pageData.subname}</div>
                 <div className="TypeName">{pageData.name}</div>
-                <img className="TypeImg" alt="auditorium_img" src="/img/auditorium.png" />
+                <img className="TypeImg" alt="auditorium_img" src={pageData.img_path} />
                 <div className="SameMbti">전체 테스트 참여자 중 7.37%가 같은 유형입니다.</div>
+
             </div>
             {/*외향, 내향 등의 비율을 나타내는 부분 */}
             <div className="ResultFrame">
@@ -36,6 +39,8 @@ function Result() {
                     : <div></div>}
                 <Chemistry />
                 <ResultOthers />
+                <button className="AllTypesButton" onClick={() => navigate("/viewAllResult")}>모든 유형 보러가기</button>
+                <button className="ReTestButton" onClick={() => navigate("/")}>테스트 다시하기</button>
                 <Logo />
             </div>
         </div >
