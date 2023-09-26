5e3124ae0ede454298477a503ce3e87bdcfc6e81
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-09-27 00:08:59 +0900

    section 부분 비율 맞춰 나오도록 수정

--- client/src/components/views/Results/Result.js
+++ client/src/components/views/Results/Result.js
@@ -3,17 +3,43 @@ import ResultOthers from "./ResultOthers";
 import Logo from "../../Footer/Logo";
 import { useParams } from "react-router-dom";
 import resultData from "./results.json";
+import { useLocation } from "react-router-dom";
+// import html2canvas from 'html2canvas';
 
 import { useNavigate } from "react-router-dom";
 function Result() {
+    const location = useLocation();
     let params = useParams();
     let pageData = resultData.results[params.id - 1];
+    let type1 = location.state[0]
+    let type2 = location.state[1]
+    let type3 = location.state[2]
+    let type4 = location.state[3]
     // console.log(params.id)
+    // let captureDiv = document.querySelector('.ResultImageCapture');  
     const navigate = useNavigate();
+    // function DownImg() {
+    //     html2canvas(captureDiv).then(canvas => {
+    //         saveImg(canvas.toDataURL('image/jpg'), 'image.jpg');
+    //     });
+    // }
+    // function CopyImg() {
+    //     html2canvas(captureDiv).then(canvas => {
+    //         window.copy(canvas);
+    //     });
+    // }
+    // const saveImg = (uri, filename) => {
+    //     let link = document.createElement('a');
+    //     document.body.appendChild(link);
+    //     link.href = uri;
+    //     link.download = filename;
+    //     link.click();
+
+    //     document.body.removeChild(link);
+    // };
     return (
         <div className="ResultPage">
             <div className="ResultImageFrame">
-
                 <span className="ResultTitle">나와 어울리는 이화여자대학교 건물은</span>
                 <div className="ResultMiddletext">{pageData.subname}</div>
                 <div className="TypeName">{pageData.name}</div>
@@ -24,12 +50,11 @@ function Result() {
             {/*외향, 내향 등의 비율을 나타내는 부분 */}
             <div className="ResultFrame">
                 <div className="ResultPercent">
-                    <div className="EAndI"></div>
-                    <div className="NAndS"></div>
-                    <div className="TAndF"></div>
-                    <div className="PAndJ"></div>
+                    <img className="ResultPercentEach" src={`/img/ResultSection/type1_section` + type1 + '.png'} />
+                    <img className="ResultPercentEach" src={`/img/ResultSection/type2_section` + type2 + '.png'} />
+                    <img className="ResultPercentEach" src={`/img/ResultSection/type3_section` + type3 + '.png'} />
+                    <img className="ResultPercentEach" src={`/img/ResultSection/type4_section` + type4 + '.png'} />
                 </div>
-                {console.log(pageData.text)};
                 {pageData.text ? pageData.text.map((eachText, idx) =>
                     <div div className="ResultText" key={idx} >
                         <div>-</div>
@@ -40,13 +65,13 @@ function Result() {
                 <div className="ChemistryPage">
                     <div className="Chemistries">
                         <div className="ChemiTitle">환상의 케미</div>
-                        <img className="ChemiImg" alt="환상의 케미 이미지" src="/img/ResultPage/Building/auditorium.png"></img>
+                        <img className="ChemiImg" alt="환상의 케미 이미지" src={pageData.img_path}></img>
                         <div className="ChemiMiddle">자유로운 영혼</div>
                         <div className="ChemiName">조형예술관</div>
                     </div>
                     <div className="Chemistries">
                         <div className="ChemiTitle">환장의 케미</div>
-                        <img className="ChemiImg" alt="환장의 케미 이미지" src="/img/ResultPage/Building/auditorium.png"></img>
+                        <img className="ChemiImg" alt="환장의 케미 이미지" src={pageData.img_path}></img>
                         <div className="ChemiMiddle">자유로운 영혼</div>
                         <div className="ChemiName">조형예술관</div>
                     </div>
