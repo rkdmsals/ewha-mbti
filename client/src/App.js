b44b45af31c23b0fe1de0fc683f62a70dd3192f3
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-09-26 18:05:00 +0900

    케미 부분 Result.js에 포함, 퍼센트 부분 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import { Router } from "express";
 import Home from "./components/views/HomePage/Home";
 import Result from "./components/views/Results/Result";
-import Chemistry from "./components/views/Results/Chemistry";
 import ViewAllResult from "./components/views/ViewAllResult/ViewAllResult";
 import MbtiMain from "./components/views/mbtimain/mbtimain";
 import Test from "./components/views/TestPage/Test";
@@ -15,7 +14,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<MbtiMain />} />
-          <Route exact path="/Chemi" element={<Chemistry />} />
           <Route exact path="/test" element={<Test />} />
           <Route exact path="/test/result/:id" element={<Result />} />
           <Route exact path="/viewAllResult" element={<ViewAllResult />} />
--- client/src/components/views/Results/Result.js
+++ client/src/components/views/Results/Result.js
@@ -1,5 +1,4 @@
 import "./Result.css";
-import Chemistry from "./Chemistry";
 import ResultOthers from "./ResultOthers";
 import Logo from "../../Footer/Logo";
 import { useParams } from "react-router-dom";
@@ -19,7 +18,7 @@ function Result() {
                 <div className="ResultMiddletext">{pageData.subname}</div>
                 <div className="TypeName">{pageData.name}</div>
                 <img className="TypeImg" alt="auditorium_img" src={pageData.img_path} />
-                <div className="SameMbti">전체 테스트 참여자 중 7.37%가 같은 유형입니다.</div>
+                {/* <div className="SameMbti">전체 테스트 참여자 중 7.37%가 같은 유형입니다.</div> */}
 
             </div>
             {/*외향, 내향 등의 비율을 나타내는 부분 */}
@@ -37,7 +36,21 @@ function Result() {
                         <div className="resultEachText">{eachText}</div>
                     </div>)
                     : <div></div>}
-                <Chemistry />
+
+                <div className="ChemistryPage">
+                    <div className="Chemistries">
+                        <div className="ChemiTitle">환상의 케미</div>
+                        <img className="ChemiImg" alt="환상의 케미 이미지" src="/img/ResultPage/Building/auditorium.png"></img>
+                        <div className="ChemiMiddle">자유로운 영혼</div>
+                        <div className="ChemiName">조형예술관</div>
+                    </div>
+                    <div className="Chemistries">
+                        <div className="ChemiTitle">환장의 케미</div>
+                        <img className="ChemiImg" alt="환장의 케미 이미지" src="/img/ResultPage/Building/auditorium.png"></img>
+                        <div className="ChemiMiddle">자유로운 영혼</div>
+                        <div className="ChemiName">조형예술관</div>
+                    </div>
+                </div>
                 <ResultOthers />
                 <button className="AllTypesButton" onClick={() => navigate("/viewAllResult")}>모든 유형 보러가기</button>
                 <button className="ReTestButton" onClick={() => navigate("/")}>테스트 다시하기</button>
