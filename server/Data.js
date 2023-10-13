9905d065563b27c2d91e802d8bcbcc43b3a65abf
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-10-13 21:20:32 +0900

    사용되지 않는 서버 삭제

--- server/Data.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const dataSchema = mongoose.Schema({
-    id: { type: mongoose.Schema.Types.ObjectId, },
-    MBTI: { type: Number, require: true },
-})
-
-const Data = mongoose.model("Data", dataSchema);
-module.exports = { Data };
